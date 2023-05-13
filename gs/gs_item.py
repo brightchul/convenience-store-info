982a123e4a270402381e6b2b8f46201e027f8e08
hoon <57323359+brightchul@users.noreply.github.com>
2023-05-14 01:51:22 +0900

    Remove dotenv  (#2)
    * feat: remove dotenv in gs_item.py
    * feat: remove dotenv in cu_item.py
    * feat: remove python-dotenv
    * docs: update README

--- gs/gs_item.py
+++ gs/gs_item.py
@@ -1,18 +1,7 @@
-import os
-from dotenv import load_dotenv
 import requests
 import json
-import time
+from bs4 import BeautifulSoup
 
-
-load_dotenv()
-
-GS_TOKEN = os.environ["GS_TOKEN"]
-GS_SESSION = os.environ["GS_SESSION"]
-
-
-URL = f'http://gs25.gsretail.com/gscvs/ko/products/event-goods-search?CSRFToken={GS_TOKEN}'
-COOKIES = {"JSESSIONID": GS_SESSION}
 HEADERS = {
     "Accept": "application/json, text/javascript, */*; q=0.01",
     "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -22,12 +11,39 @@ HEADERS = {
     "Referer": "http://gs25.gsretail.com/gscvs/ko/products/event-goods"
 }
 
+PAGE_SIZE = 16
 
-def get_raw_data_text(page_index, page_size):
+
+def find_in_arr(target: str, arr: list[str]):
+    for one in arr:
+        if one.find(target) > -1:
+            return one
+    return None
+
+
+def get_session_token():
+    res = requests.get(
+        "http://gs25.gsretail.com/gscvs/ko/products/event-goods")
+    soup = BeautifulSoup(res.text, "html.parser")
+    cookie_list = res.headers["Set-Cookie"].split("; ")
+    session = find_in_arr("JSESSIONID", cookie_list).split("=")[1]
+    token = soup.find("input", attrs={"name": "CSRFToken"}).attrs['value']
+
+    return {"token": token, "session": session}
+
+
+def init_setting_data():
+    session_token = get_session_token()
+
+    return {"url": f'http://gs25.gsretail.com/gscvs/ko/products/event-goods-search?CSRFToken={session_token["token"]}',
+            "cookies": {"JSESSIONID": session_token["session"]}}
+
+
+def get_raw_data_text(url, cookies, page_index, page_size):
     dataParam = {
         "pageNum": page_index, "pageSize": page_size, "searchType": "", "searchWord": "", "parameterList": "TOTAL"
     }
-    req = requests.post(url=URL, cookies=COOKIES,
+    req = requests.post(url=url, cookies=cookies,
                         data=dataParam, headers=HEADERS)
 
     if req.ok:
@@ -42,35 +58,38 @@ def convert_info(json_data):
 
 
 def get_item_info(info):
-    print(info)
     return info["results"]
 
 
-def get_number_of_pages(info):
-    return info["pagination"]["numberOfPages"]
+def get_item_info_list(url, cookies):
+    total_list = []
+    page_number = 1
+
+    while (True):
+        print(page_number)
+        raw_data = get_raw_data_text(url, cookies, page_number, PAGE_SIZE)
+        info_data = convert_info(raw_data)
+        page_item_info = get_item_info(info_data)
+
+        if len(page_item_info) == 0:
+            break
+
+        total_list.extend(page_item_info)
+        page_number += 1
+
+    return total_list
+
+
+def write_json(data, file_name):
+    json_data = json.dumps(data, ensure_ascii=False)
+    with open(file_name, 'w', encoding='utf-8') as file:
+        file.write(json_data)
 
 
 def run():
-
-    total_list = []
-
-    raw_data = get_raw_data_text(1, 16)
-    info_data = convert_info(raw_data)
-    page1_item_info = get_item_info(info_data)
-    total_list.extend(page1_item_info)
-
-    max_page_number = get_number_of_pages(info_data)
-
-    for page_number in range(2, max_page_number):
-        raw_data = get_raw_data_text(page_number, 16)
-        info_data = convert_info(raw_data)
-        page_item_info = get_item_info(info_data)
-        total_list.extend(page_item_info)
-        time.sleep(0.1)
-
-    json_data = json.dumps(total_list, ensure_ascii=False)
-    with open("gs_item.json", 'w', encoding='utf-8') as file:
-        file.write(json_data)
+    setting_data = init_setting_data()
+    result = get_item_info_list(setting_data["url"], setting_data["cookies"])
+    write_json(result, "gs_item.json")
 
 
 if __name__ == "__main__":
