2a8eef90fb5ba39700f717872cab43d503c0800c
brightchul <abhbtbb1@gmail.com>
2023-05-17 23:05:43 +0900

    feat: add gs event crawling

---
+++ gs/gs_event.py
@@ -0,0 +1,98 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import json
+
+HEADERS = {
+    "Accept": "application/json, text/javascript, */*; q=0.01",
+    "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Connection": "keep-alive",
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "Origin": "http://gs25.gsretail.com",
+    "Referer": "http://gs25.gsretail.com/gscvs/ko/products/event-goods"
+}
+
+domain_url = "http://gs25.gsretail.com"
+event_page_url = "http://gs25.gsretail.com/gscvs/ko/customer-engagement/event/current-events"
+
+
+def find_in_arr(target: str, arr: list[str]):
+    for one in arr:
+        if one.find(target) > -1:
+            return one
+    return None
+
+
+def get_session_token():
+    res = requests.get(
+        "http://gs25.gsretail.com/gscvs/ko/products/event-goods")
+    soup = BeautifulSoup(res.text, "html.parser")
+    cookie_list = res.headers["Set-Cookie"].split("; ")
+    session = find_in_arr("JSESSIONID", cookie_list).split("=")[1]
+    token = soup.find("input", attrs={"name": "CSRFToken"}).attrs['value']
+
+    return {"token": token, "session": session}
+
+
+def init_setting_data():
+    session_token = get_session_token()
+
+    return {"url": f'http://gs25.gsretail.com/board/boardList?CSRFToken={session_token["token"]}',
+            "cookies": {"JSESSIONID": session_token["session"]}}
+
+
+def get_raw_data_text(url, cookies, page_index, page_size):
+    dataParam = {
+        "pageNum": page_index, "pageSize": page_size, "modelName": "event", "parameterList": "brandCode:GS25@!@eventFlag:CURRENT"
+    }
+    res = requests.post(url=url, cookies=cookies,
+                        data=dataParam, headers=HEADERS)
+
+    if res.ok:
+        return res.text
+    else:
+        return None
+
+
+def convert_info(json_data):
+    info_data = json.loads(json.loads(json_data))
+    return info_data
+
+
+def get_item_info(info):
+    return info["results"]
+
+
+def get_item_info_list(url, cookies):
+    total_list = []
+    page_number = 1
+
+    while (True):
+        print(page_number)
+        raw_data = get_raw_data_text(url, cookies, page_number, 10)
+        info_data = convert_info(raw_data)
+        page_item_info = get_item_info(info_data)
+
+        if len(page_item_info) == 0:
+            break
+
+        total_list.extend(page_item_info)
+        page_number += 1
+
+    return total_list
+
+
+def write_json(data, file_name):
+    json_data = json.dumps(data, ensure_ascii=False)
+    with open(file_name, 'w', encoding='utf-8') as file:
+        file.write(json_data)
+
+
+def run():
+    setting_data = init_setting_data()
+    result = get_item_info_list(setting_data["url"], setting_data["cookies"])
+    write_json(result, "gs_event.json")
+
+
+if __name__ == "__main__":
+    run()
