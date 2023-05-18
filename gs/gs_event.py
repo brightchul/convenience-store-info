90ab414a8ba4abd35f68ff18d010accbef965c82
hoon <57323359+brightchul@users.noreply.github.com>
2023-05-18 17:57:36 +0900

    Add event crawling (cu, gs, emart24) (#3)
    * feat: add cu event crawling
    * feat: add emart24 event crawling
    * feat: add gs event crawling
    * refactor: modularize common functionality

--- gs/gs_event.py
+++ gs/gs_event.py
@@ -1,7 +1,6 @@
 import requests
 
-from gs_common import convert_info, get_item_info, init_setting_data, write_json
-
+from gs_common import convert_info, get_item_info,  init_setting_data, write_json
 
 HEADERS = {
     "Accept": "application/json, text/javascript, */*; q=0.01",
@@ -12,21 +11,19 @@ HEADERS = {
     "Referer": "http://gs25.gsretail.com/gscvs/ko/products/event-goods"
 }
 
-PAGE_SIZE = 16
-
-TARGET_URL = "http://gs25.gsretail.com/gscvs/ko/products/event-goods-search"
+DOMAIN_URL = "http://gs25.gsretail.com/board/boardList"
 
 
 def get_raw_data_text(url, cookies, page_index, page_size):
     dataParam = {
-        "pageNum": page_index, "pageSize": page_size,
-        "searchType": "", "searchWord": "", "parameterList": "TOTAL"
+        "pageNum": page_index, "pageSize": page_size, "modelName": "event",
+        "parameterList": "brandCode:GS25@!@eventFlag:CURRENT"
     }
-    req = requests.post(url=url, cookies=cookies,
+    res = requests.post(url=url, cookies=cookies,
                         data=dataParam, headers=HEADERS)
 
-    if req.ok:
-        return req.text
+    if res.ok:
+        return res.text
     else:
         return None
 
@@ -37,7 +34,7 @@ def get_item_info_list(url, cookies):
 
     while (True):
         print(page_number)
-        raw_data = get_raw_data_text(url, cookies, page_number, PAGE_SIZE)
+        raw_data = get_raw_data_text(url, cookies, page_number, 10)
         info_data = convert_info(raw_data)
         page_item_info = get_item_info(info_data)
 
@@ -51,9 +48,9 @@ def get_item_info_list(url, cookies):
 
 
 def run():
-    setting_data = init_setting_data(TARGET_URL)
+    setting_data = init_setting_data(DOMAIN_URL)
     result = get_item_info_list(setting_data["url"], setting_data["cookies"])
-    write_json(result, "gs_item.json")
+    write_json(result, "gs_event.json")
 
 
 if __name__ == "__main__":
